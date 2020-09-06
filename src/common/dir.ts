db7971d836e204af0efcb9db03ab0176ca5d6e13
YouXin <954382491@qq.com>
2020-09-06 20:07:45 +0800

    fix:线上文章不能随发布更新的问题

--- src/common/dir.ts
+++ src/common/dir.ts
@@ -2,4 +2,4 @@ declare var __posts_pdir__: string;
 declare var __posts_dir__: string;
 
 export const __posts_root__ = __posts_pdir__ + __posts_dir__ + '/';
-export const __posts_db__ = __posts_root__ + 'db.json';
+export const __posts_db__ = __posts_root__ + `db.json?var=${Date.now()}`;
