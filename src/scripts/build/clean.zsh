687d45293470f6e5d10e48b817767ff4a2ca2fd3
又心真人 <954382491@qq.com>
2021-04-30 18:44:00 +0800

    bundle

--- src/scripts/build/clean.zsh
+++ src/scripts/build/clean.zsh
@@ -3,6 +3,7 @@
 for arg in "$@"; do
     case "${arg}" in
       b) rm -rf public/posts public/v1 public/v2 build/gen/prod ;;
+      bt) rm -rf public/posts public/v1 build/gen/prod ;;
       d) rm -rf build/gen/dev build/posts ;;
       t) rm -rf build/gen/dev build/tests/markdown-it ;;
     esac
