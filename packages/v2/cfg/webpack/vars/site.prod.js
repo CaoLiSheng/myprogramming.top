727f128957d2c2c7b62984c01b371cd9110fa669
曹力升 <caoyouxin8@gmail.com>
2021-09-02 09:56:52 +0800

    bundle v1/v2

--- packages/v2/cfg/webpack/vars/site.prod.js
+++ packages/v2/cfg/webpack/vars/site.prod.js
@@ -2,8 +2,8 @@ const webpack = require ( 'webpack' );
 
 module.exports = new webpack.DefinePlugin ( {
   __production__  : JSON.stringify ( true ),
-  __resource_dir__: JSON.stringify ( '../resources/' ),
-  __posts_root__  : JSON.stringify ( '../posts/' ),
-  __portal_to_v1__: JSON.stringify ( '../v1/' ),
-  __portal_to_v2__: JSON.stringify ( '../v2/' ),
+  __resource_dir__: JSON.stringify ( '//www.myprogramming.top/resources/' ),
+  __posts_root__  : JSON.stringify ( '//www.myprogramming.top/posts/' ),
+  __portal_to_v1__: JSON.stringify ( '//www.myprogramming.top/v1/' ),
+  __portal_to_v2__: JSON.stringify ( '//www.myprogramming.top/v2/' ),
 } );
