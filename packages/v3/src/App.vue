eae51f7873e70274b01d194e35dc85ad96329546
曹力升 <caoyouxin8@gmail.com>
2022-06-06 18:52:17 +0800

    v3 commit

--- packages/v3/src/App.vue
+++ packages/v3/src/App.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
   .layout
-    
+    .layout-content
+      .layout-content-left
+      .layout-content-right
+    .layout-header
+      .layout-logo
+      .layout-nav
 </template>
 
 <script lang='ts'>
@@ -37,5 +42,34 @@ export default Vue.extend({
 .layout {
   position: relative;
 
+  .layout-content {
+    @include flex-center;
+
+    height: 66.6vh;
+    background-color: #eee;
+
+    .layout-content-left {
+      width: 33.4vw;
+    }
+
+    .layout-content-right {
+      width: 66.6vw;
+    }
+  }
+
+  .layout-header {
+    @include flex-center;
+
+    height: 33.4vh;
+    background-color: rgb(10, 126, 64);
+
+    .layout-logo {
+      width: 33.4vw;
+    }
+
+    .layout-nav {
+      width: 66.6vw;
+    }
+  }
 }
 </style>
